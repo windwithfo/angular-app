fa2fd704afaa9a415d45de50caba926a6cdad8fc
dongkunshan <windwithfo@yeah.net>
2022-08-10 16:20:50 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestComponent } from './test/test.component';
+import { IndexComponent } from './index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: IndexComponent
+},
+{
+  path: 'test',
+  component: TestComponent
+}]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
